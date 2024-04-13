847fbc6d8013782f09c4d44910b0c873679309a6
samsonemeje <samsonemeje@gmail.com>
2024-04-13 15:41:29 +0100

    change aws config name

--- providers.tf
+++ providers.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "portfolio-labs"
+  profile = "default"
   # Configuration options
 }
 
 provider "aws" {
   alias   = "west"
   region  = "us-west-1"
-  profile = "portfolio-labs"
+  profile = "default"
 }
 

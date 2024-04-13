e94f3501c38f4802846326af41c6931cf20c73c0
samsonemeje <samsonemeje@gmail.com>
2024-04-13 15:37:43 +0100

    add ami id for us-east-1 and us-west-1

--- providers.tf
+++ providers.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 5.31.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  profile = "Helen"
+  region  = "us-east-1"
+  profile = "portfolio-labs"
   # Configuration options
 }
 
 provider "aws" {
-  alias = "west"
-  region = "us-west-1"
-  profile = "Helen"
+  alias   = "west"
+  region  = "us-west-1"
+  profile = "portfolio-labs"
 }
 

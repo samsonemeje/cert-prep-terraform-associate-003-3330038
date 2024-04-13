64abb9d172eff0f9e2f1887f167166428ea1b60e
samsonemeje <samsonemeje@gmail.com>
2024-04-13 15:53:54 +0100

    add vpc and subnet configuration

--- main.tf
+++ main.tf
@@ -1,17 +1,55 @@
+# resource "aws_instance" "ec2-instance1" {
+#   ami           = "ami-051f8a213df8bc089"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "demo-server1"
+#   }
+# }
+
+
+# resource "aws_instance" "ec2-Instance2" {
+#   provider      = aws.west
+#   ami           = "ami-0b990d3cfca306617" # AMI for us-west-1
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "demo-server2"
+#   }
+# }
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-1a"
+}
+
 resource "aws_instance" "ec2-instance1" {
   ami           = "ami-051f8a213df8bc089"
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet1.id
 
   tags = {
     Name = "demo-server1"
   }
 }
 
-
 resource "aws_instance" "ec2-Instance2" {
   provider      = aws.west
   ami           = "ami-0b990d3cfca306617" # AMI for us-west-1
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet2.id
 
   tags = {
     Name = "demo-server2"

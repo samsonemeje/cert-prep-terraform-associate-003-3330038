6e9db0ea78ecb8f49f6340346344a1b4a8a50fe2
samsonemeje <samsonemeje@gmail.com>
2024-04-13 18:40:19 +0100

    modified

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "aws_subnet" "subnet1" {
 }
 
 resource "aws_subnet" "subnet2" {
+  provider          = aws.west
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-1a"
